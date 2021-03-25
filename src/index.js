cd2250df0729a99b90d876a3623780dc2a391ed7
Mrfingerstyle <FE_liuyoung@163.com>
2021-03-25 08:47:12 +0800

    update index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import png from './assets/1.png'
 console.log(x)
 console.log('hi--');
 
-console.log(png);
-const div = document.getElementById('app')
-const img = document.createElement('img')
-img.src = png
-console.log(img);
-div.appendChild(img)
+// console.log(png);
+// const div = document.getElementById('app')
+// const img = document.createElement('img')
+// img.src = png
+// console.log(img);
+// div.appendChild(img)
 
 const btn = document.createElement('button')
 btn.innerText = 'lazy loader'
